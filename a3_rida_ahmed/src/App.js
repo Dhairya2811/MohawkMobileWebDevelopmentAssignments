8ed1d5eb4d530c5c8ef4a0e8a5e7f79fe88a9320
dhairya2711 <dsdrago27@gmail.com>
2023-02-17 17:38:05 -0500

    except roll btn, everything is done

--- a3_rida_ahmed/src/App.js
+++ a3_rida_ahmed/src/App.js
@@ -1,15 +1,116 @@
 import Dice from './components/dice/dice';
+import { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
-  var diceArr = [6, 4, 3, 5, 9, 20];
+  const [diceNumber, setDiceNumber] = useState(1);
+  const [diceSide, setDiceSide] = useState(6);
+  const [diceVal, setDiceVal] = useState([]);
+
+  
+  // roll btn click
+  var rollDice = ()=>{
+    // for(let i = 0; i<diceNumber; i++){
+    //   if(diceVal.length == diceNumber){
+    //     dices = diceVal;
+    //     for(let i = 0; i<diceNumber; i++){
+    //       let randomNumber = Math.floor(Math.random()*(diceSide))+1;
+    //       dices[i] = randomNumber;
+    //     }
+    //     setDiceVal(dices);
+    //   }else{
+        
+    //   }
+    // }
+    setDiceVal([]);
+    // var dices = [];
+      for(let i=0; i<diceNumber; i++){
+        let randomNumber = Math.floor(Math.random()*(diceSide))+1;
+        setDiceVal([...diceVal, randomNumber]);
+      }
+    // setDiceVal(dices);
+  };
+
+  useEffect(()=>{
+    rollDice();
+  }, [diceNumber, diceSide]);
+
+  // increase number of dice
+  var increaseDice = ()=>{
+    setDiceNumber(diceNumber+1);
+  };
+
+  // decrease number of dice
+  var decreaseDice = ()=>{
+    if(diceNumber>1){
+      setDiceNumber(diceNumber-1);
+    }
+  };
+
+  // increase number of side
+  var increaseSide = ()=>{
+    if(diceSide<20){
+      setDiceSide(diceSide+1);
+    }
+  };
+
+  // decrease number of side
+  var decreaseSide = ()=>{
+    if(diceSide>1){
+      setDiceSide(diceSide-1);
+    }
+  };
+
+  // reset btn click
+  var resetBtnClick = ()=>{
+    setDiceNumber(1);
+    setDiceSide(6);
+    setDiceVal([])
+  };
+
+
+  var key = 0;
   return (
     <div className="App">
-      {diceArr.map(element => 
-        {
-          return <Dice number={element}/>
-        })
-      }
+      <div className="DiceDisplay">
+        {console.log(diceVal)}
+        {diceVal.map(element => 
+          { key = key + 1;
+            return <Dice key={key} maxSide={diceSide} number={element}/>
+          })
+        }
+      </div>
+
+
+      {/* buttons section */}
+      <div id='bunchOfButtons'>
+        {/* dices btn */}
+        <div id="increaseDice" className='buttons'>
+          <div className='btnContainer'>
+            <button className='btn' id='increaseDice' onClick={increaseDice.bind(this)} style={{transform: "rotate(-90deg)"}}>&#10140;</button>
+          </div>
+          <div className='numberDisplay'><strong>Number of dice:</strong> <span className='displayNumber'>{diceNumber}</span></div>
+          <div className='btnContainer'>
+            <button className='btn' id='decreaseDice' onClick={decreaseDice.bind(this)} style={{transform: "rotate(90deg)"}}>&#10140;</button>
+          </div>
+        </div>
+        {/* roll btn */}
+        {/* <input type="button" value="Roll" className='btn' onClick={rollDice.bind(this)} /> */}
+        {/* reset btn */}
+        <input type="button" value="Reset" className='btn' onClick={resetBtnClick.bind(this)} />
+        {/* sides btn */}
+        <div id="increaseSide" className='buttons'>
+          <div className='btnContainer'>
+            <button className='btn' onClick={increaseSide.bind(this)}  style={{transform: "rotate(-90deg)"}}>&#10140;</button>
+          </div>
+          <div className='numberDisplay'><strong>Number of side:</strong> <span className='displayNumber'>{diceSide}</span></div>
+          <div className='btnContainer'>
+            <button className='btn' onClick={decreaseSide.bind(this)}  style={{transform: "rotate(90deg)"}}>&#10140;</button>
+          </div>
+        </div>
+      </div>
+
+
     </div>
   );
 }
--- a3_rida_ahmed/src/components/dice/dice.js
+++ a3_rida_ahmed/src/components/dice/dice.js
@@ -1,10 +1,21 @@
-import React from 'react';
+import React, {useState} from 'react';
+
 import styles from './dice.module.css';
 
 const Dice = (props) => {
+  var [displayNumber, setDisplayNumber] = useState(props.number);
+
+  var increaseNumOnDice = ()=>{
+    if(displayNumber<props.maxSide){
+      setDisplayNumber(displayNumber+=1)
+    }else{
+      setDisplayNumber(1);
+    }
+  };
+
   return (
-    <div className={styles.Dice}>
-      {props.number}
+    <div onClick={increaseNumOnDice.bind(this)} className={styles.Dice}>
+      {displayNumber}
     </div>
   );
 };
