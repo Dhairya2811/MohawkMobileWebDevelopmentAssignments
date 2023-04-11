e948b8f708b7858eac68980f8496882b7c8ebde9
Dhairya2811 <sonidhairya2811@gmail.com>
2023-04-11 03:34:39 -0400

    created UI for Login and Signup

--- frontend/App.js
+++ frontend/App.js
@@ -1,8 +1,11 @@
 import { StyleSheet, Text, View } from 'react-native';
 import Login from "./Component/Login"
 import Signup from './Component/Signup';
+import { useState } from 'react';
 
 export default function App() {
+  const [user, setUser] = useState();
+
   var path = window.location.pathname;
   return (
     <View style={styles.container}>
--- frontend/Component/Signup.js
+++ frontend/Component/Signup.js
@@ -1,8 +1,12 @@
 import { StyleSheet, Text, View } from 'react-native';
 import { useState } from 'react';
+import { TextInput, Button } from '@react-native-material/core';
 
-export default function Signup() {
+export default function Login() {
     const [res, setRes] = useState();
+    const [username, setUsername] = useState();
+    const [password, setPassword] = useState();
+    const [confirmPassword, setConfirmPassword] = useState();
 
     fetch("http://localhost:3000/signup")
     .then(res => res.text())
@@ -10,7 +14,38 @@ export default function Signup() {
 
     return (
         <View style={styles.container}>
-            <Text>{ res }</Text>
+            <View style={styles.form}>
+                <TextInput
+                    label='User Name'
+                    onChange={setUsername}
+                    style={styles.usernameText}
+                />
+                <TextInput
+                    label='Password'
+                    onChange={setPassword}
+                    secureTextEntry={true}
+                    style={styles.passwordText}
+                />
+                <TextInput
+                    label='Confirm Password'
+                    onChange={setConfirmPassword}
+                    secureTextEntry={true}
+                    style={styles.passwordText}
+                />
+                <View style={styles.buttons}>
+                    <Button 
+                        variant='text'
+                        title="Login" 
+                        color="#3377ff"
+                    />
+                    <Button 
+                        title="Submit" 
+                        color="#3377ff"
+                        tintColor='white'
+                        titleStyle={styles.submitBtn}
+                    />
+                </View>
+            </View>
         </View>
     );
 }
@@ -21,5 +56,26 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    width: "80%",
+    marginHorizontal: "10%",
   },
+  form: {
+    width: "50%",
+    minWidth: 200,
+    maxWidth: 500,
+  },
+  usernameText: {
+
+  },
+  passwordText: {
+    marginVertical: 10
+  }, 
+  buttons: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  }, 
+  submitBtn: {
+    fontWeight: "bold"
+  } 
 });
